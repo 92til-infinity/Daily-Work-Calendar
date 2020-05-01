da1784d00b126e097d37c90c0ae0d40aefb41fce
Scott G <glover.scott92@gmail.com>
2020-04-30 18:31:31 -0700

    got the list to append to the right day

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,3 +1,5 @@
+// i comment out 
+
 // Create a new list item when clicking on the "Add" button
 function newTodoItem() {
   var li = document.createElement("li");
@@ -7,51 +9,51 @@ function newTodoItem() {
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementsByClassName("todoItem").appendChild(li);
+    console.log(document.getElementsByClassName(("todoItem")[0]));
+    document.getElementsByClassName("todoItem")[0].appendChild(li);
     // append to active day
-    // change appended item to set color
   }
 
-  document.getElementById("myInput").value = "";
+  // document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+  // var span = document.createElement("SPAN");
+  // var txt = document.createTextNode("\u00D7");
+  // span.className = "close";
+  // span.appendChild(txt);
+  // li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  // for (i = 0; i < close.length; i++) {
+  //   close[i].onclick = function () {
+  //     var div = this.parentElement;
+  //     div.style.display = "none";
+  //   }
+  // }
 }
 
 
 
 
 // Create a "close" button and append it to each list item
-var closeButton = document.getElementsByClassName("todoItem");
+// var closeButton = document.getElementsByClassName("todoItem");
 
-for (var i = 0; i < closeButton.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  closeButton[i].appendChild(span);
-}
+// for (var i = 0; i < closeButton.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   closeButton[i].appendChild(span);
+// }
 
 //Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+// var close = document.getElementsByClassName("close");
 
-for (var i = 0; i < close.length; i++) {
+// for (var i = 0; i < close.length; i++) {
 
-  close[i].onclick = function () {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+//   close[i].onclick = function () {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
 
 // Add a "checked" symbol when clicking on a list item
 // var list = document.querySelector('ul');
@@ -85,10 +87,10 @@ function filterFunction() {
 function todaysDate() {
   var currentDate = moment().format("MMM Do YY");
   $(".today").text(currentDate);
-  console.log(currentDate);
-  console.log(currentDate.split(" ")[1].split("th")[0]);
+  // console.log(currentDate);
+  // console.log(currentDate.split(" ")[1].split("th")[0]);
   var dateOutput = currentDate.split(" ")[1].split("th")[0];
-  console.log(typeof dateOutput);
+  // console.log(typeof dateOutput);
   return dateOutput;
 }
 todaysDate();
